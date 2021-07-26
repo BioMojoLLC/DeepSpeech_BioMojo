5646ad045bea2dab1bce1bcf6d9216a12309d617
rdhurlbu <rdhurlbu@ncsu.edu>
2021-07-26 16:18:21 -0400

    Updated documentation for the scraping script

--- scripts/hacky_web_scraper.py
+++ scripts/hacky_web_scraper.py
@@ -2,12 +2,43 @@
 """
 Created on Fri Jul 23 09:07:23 2021
 
+Script for collecting and saving text related to key words. 
+
+PARAMETERS:
+    source: 
+        This is the general subject matter. Your input file for this script 
+        needs to be named in the format "raw-{source}". Where source is the 
+        general subject matter the vocabulary words surround. 
+            
+    key_words: 
+        This is a list of words to select upon. Sentences will be saved and
+        if entered on it's own line if they contain at east one word in this 
+        list. 
+
+RETURNS:
+    The output file of the program will save to "cleaned-{source}.txt".
+
+
+Usage:
+    The most simple usage of this script is to perform the following steps:
+        1. Find a web page you want to scrape from. 
+        2. Ctrl + A , Ctrl + C (CMD for mac). 
+        3. Paste text into a basic text file
+        4. Repeat for as many web pages as you wish
+        5. Save file as "raw-{source}.txt" where source is your general subject matter
+        6. Open up the script, change the source variable to the source saved in the 
+            above file name.
+        7. In the key_words variable, enter the key words you want to select sentences with
+        8. Run. 
+        9. Check the outut file, remove any irrelevant or useless lines. (There will be a few)
+
 @author: Ryan Hurlbut
 """
 import re
 
-source = 'triage-related'
-key_words = [' IV ', ' IO ', 'lock', 'nasopharyngeal', 'HPMK', 'hypothermia', 'intubation', 'intubate' ]
+# CHANGE ME
+source = 'medical'
+key_words = ['medical', 'or', 'other', 'interesting', 'words' ]
 
 raw_text_file = f'raw-{source}.txt'
 
